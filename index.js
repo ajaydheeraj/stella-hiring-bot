6cb20a0580b53e3a10ced068dc9924d9e4388e25
shalini-s <shalini.sreedhar@emory.edu>
2017-10-14 16:14:56 -0400

    js

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+var template = $('#template').html();
+ // You can pass dynamic template values
+  var rendered = Mustache.render(template, {name: "Luke"});
+  $('#target').html($(<iframe src='https://webchat.botframework.com/embed/stellaHiring?s=SMG_TqAHgsw.cwA.FC0.XRvC71_cB23d6VtayQH7v3tE9T3HzNYGUowXWE5JDDY' height="400"></iframe>));
